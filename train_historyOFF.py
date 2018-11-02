4c9152a29a3f3b5ff50513b709448da46183396b
qiweiwang <qiweiwang89@gmail.com>
2018-11-02 15:25:49 -0700

    hidden layer added

--- train_historyOFF.py
+++ train_historyOFF.py
@@ -6,7 +6,7 @@ from lib.Environment import Environment
 from lib.History import History
 
 #Test Cases
-NUM_EPOCH = 3000
+NUM_EPOCH = 5000
 HORIZON = 1000
 
 USERCASE = 10
@@ -32,7 +32,7 @@ else:
     raise ValueError('invalid user number case!')
 
 #Model hyperparameters
-HIDDEN_LAYER = []
+HIDDEN_LAYER = [32] #also modify initialization_parameters(), forward_prop()
 
 BATCH_SIZE = 64
 POOLSIZE = 100000
@@ -44,6 +44,9 @@ numberOfQueues = USERCASE
 featureSize = 2*numberOfQueues+1
 numberOfNeurons = [featureSize] + HIDDEN_LAYER +[numberOfActions]
 
+lossLogName = 'loss_32_5k_large.csv'
+rewardLogName = 'reward_32_5k_large.csv'
+
 def qEstimate(R, X, parameters, BETA):
     idx = tf.constant([featureSize-1])
     simTime = tf.gather(X, idx)
@@ -54,8 +57,8 @@ def initialization_parameters(featureSize, numberOfNeurons):
 
     W1 = tf.get_variable("W1", [numberOfNeurons[1],numberOfNeurons[0]], initializer = tf.contrib.layers.xavier_initializer())
     b1 = tf.get_variable("b1", [numberOfNeurons[1],1], initializer = tf.zeros_initializer())
-    # W2 = tf.get_variable("W2", [numberOfNeurons[1],numberOfNeurons[0]], initializer = tf.contrib.layers.xavier_initializer())
-    # b2 = tf.get_variable("b2", [numberOfNeurons[1],1], initializer = tf.zeros_initializer())
+    W2 = tf.get_variable("W2", [numberOfNeurons[2],numberOfNeurons[1]], initializer = tf.contrib.layers.xavier_initializer())
+    b2 = tf.get_variable("b2", [numberOfNeurons[2],1], initializer = tf.zeros_initializer())
     # W3 = tf.get_variable("W3", [numberOfNeurons[2],numberOfNeurons[1]], initializer = tf.contrib.layers.xavier_initializer())
     # b3 = tf.get_variable("b3", [numberOfNeurons[2],1], initializer = tf.zeros_initializer())
     # W4 = tf.get_variable("W4", [numberOfNeurons[3],numberOfNeurons[2]], initializer = tf.contrib.layers.xavier_initializer())
@@ -63,8 +66,8 @@ def initialization_parameters(featureSize, numberOfNeurons):
 
     parameters = {'W1': W1,
                   'b1': b1,
-                  # 'W2': W2,
-                  # 'b2': b2,
+                  'W2': W2,
+                  'b2': b2,
                   # 'W3': W3,
                   # 'b3': b3,
                   # 'W4': W4,
@@ -75,21 +78,21 @@ def initialization_parameters(featureSize, numberOfNeurons):
 def forward_prop(X, parameters):
     W1 = parameters['W1']
     b1 = parameters['b1']
-    # W2 = parameters['W2']
-    # b2 = parameters['b2']
+    W2 = parameters['W2']
+    b2 = parameters['b2']
     # W3 = parameters['W3']
     # b3 = parameters['b3']
     # W4 = parameters['W4']
     # b4 = parameters['b4']
 
     Z1 = tf.add(tf.matmul(W1, X), b1)                                              # Z1 = np.dot(W1, X) + b1
-    # A1 = tf.nn.relu(Z1)                                              # A1 = relu(Z1)
-    # Z2 = tf.add(tf.matmul(W2, A1), b2)                                             # Z2 = np.dot(W2, a1) + b2
+    A1 = tf.nn.relu(Z1)                                              # A1 = relu(Z1)
+    Z2 = tf.add(tf.matmul(W2, A1), b2)                                             # Z2 = np.dot(W2, a1) + b2
     # A2 = tf.nn.relu(Z2)                                              # A2 = relu(Z2)
     # Z3 = tf.add(tf.matmul(W3, A2), b3)                                               # Z3 = np.dot(W3,Z2) + b3
     # A3 = tf.nn.relu(Z3)
     # Z4 = tf.add(tf.matmul(W4, A3), b4)
-    End = Z1
+    End = Z2
 
     return End
 
@@ -127,7 +130,7 @@ if __name__ == "__main__":
     instant_r = tf.placeholder(tf.float32, [1, None], name = 'instant_r')
     qE = qEstimate(instant_r, x_next,parameters, BETA)
     cost = tf.reduce_mean(tf.squared_difference(qsa, qE))
-    optimizer = tf.train.AdamOptimizer(learning_rate = 0.0001).minimize(cost)
+    optimizer = tf.train.AdamOptimizer(learning_rate = 0.001).minimize(cost)
     x_glie = tf.placeholder(tf.float32, [featureSize,None], name = 'x_glie')
     z_glie = forward_prop(x_glie, parameters)
 
@@ -149,8 +152,14 @@ if __name__ == "__main__":
             action = glie(currentFi, 1, parameters)
             ev.multiSim(action)
         print('ready to learn!')
-        fd_loss = open('loss.csv','w')
-        fd_reward = open('reward.csv','w')
+        fd_loss = open(lossLogName,'w')
+        fd_reward = open(rewardLogName,'w')
+        print('Length of Horizon is {}'.format(HORIZON))
+        print('Batch_size is {}'.format(BATCH_SIZE))
+        print('Number of Epoch is {}'.format(NUM_EPOCH))
+        print('Hidden Layers:')
+        for i in HIDDEN_LAYER:
+            print(i)
         input()
 
     ### learning phase
@@ -162,7 +171,6 @@ if __name__ == "__main__":
                 startPoint = 0
                 ev.newEpochInit()
                 print(ev.currentLength)
-                # input()
             for i in range(startPoint,HORIZON):
                 historyCounter += 1
                 if i%500 == 0 and i!=0:
@@ -170,7 +178,6 @@ if __name__ == "__main__":
                     # print(ev.simTime)
                     print('cost is {}'.format(cost_run))
                     fd_loss.write(str(cost_run)+'\n')
-                # historyParsed = parseHistory(history)
                 thisFi = ev.get_current_fi()
                 explore_rate = max([500000/(500000+historyCounter), 0.1])
                 action = glie(thisFi, explore_rate, parameters)
@@ -179,10 +186,6 @@ if __name__ == "__main__":
                     fi_dic = ev.get_batch_random(BATCH_SIZE)
                 else:
                     fi_dic = ev.get_batch_mostRecent(BATCH_SIZE)
-                # print(sess.run(z, feed_dict = {x: fi_dic['this']}))
-                # print(fi_dic['a'])
-                # print(sess.run(qsa, feed_dict = {x: fi_dic['this'], actions_index: fi_dic['a']}))
-                # input()
                 _, cost_run = sess.run([optimizer,cost], feed_dict = {x_next: fi_dic['next'], instant_r: fi_dic['r'], x: fi_dic['this'], actions_index: fi_dic['a']})
             print('packetDroped: {}'.format(ev.packetDroped))
             print('packetArrived: {}'.format(ev.packetArrived))
@@ -191,4 +194,5 @@ if __name__ == "__main__":
             print('channel lost: {}'.format(ev.channelLost))
             print('user served: {}'.format(ev.served))
             fd_reward.write(str(ev.totalDiscounted)+'\n')
-        # fd.close()
+        fd_reward.close()
+        fd_loss.close()
